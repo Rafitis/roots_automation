76de138199a9bfe6c5cf4f5152e8e0f185cb6809
rafitis <rafitis00@gmail.com>
2024-10-08 20:21:14 +0200

    main app

---
+++ main.py
@@ -0,0 +1,43 @@
+from io import BytesIO
+
+import pandas as pd
+import streamlit as st
+
+from transform_csv import transform_data
+
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine="xlsxwriter")
+    df.to_excel(writer, index=False, sheet_name="Facturas")
+    writer._save()
+    processed_data = output.getvalue()
+    return processed_data
+
+
+st.set_page_config(
+    page_title="Shopify Orders to Excel",
+    page_icon="🧊",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+st.title("Shopify Orders to Excel")
+
+spectra = st.file_uploader("upload file", type={"csv", "txt"})
+if spectra is not None:
+    spectra_df = pd.read_csv(spectra)
+    final_data = transform_data(spectra_df)
+
+    st.dataframe(final_data)
+
+    doc = convert_df(final_data)
+
+    st.download_button(
+        label="Download as Excel",
+        data=doc,
+        file_name="Listado Facturas.xlsx",
+        mime="application/vnd.ms-excel",
+    )
---
+++ transform_csv.py
@@ -0,0 +1,103 @@
+import pandas as pd
+from pandas import DataFrame
+
+
+def get_data():
+    # https://drive.google.com/file/d/1kexD0y7sn6yBOoDA5VnUPOZb7HNjJ93F/view?usp=drive_link
+    return pd.read_csv("./INPUT/orders_export.csv")
+
+
+def transform_data(data: DataFrame):
+    # Elimino duplicados y los totales a cero.
+    data = data.drop_duplicates(subset=["Name"])
+    data = data.drop(data[data["Total"] == 0].index)
+
+    # Elimniar espacios en blanco
+    data["Shipping Name"] = data["Shipping Name"].replace(r"\s+", " ", regex=True)
+    data["Shipping Name"] = data["Shipping Name"].str.strip()
+
+    # Si no existe Shipping Name Coger el Billing Name
+    data["Shipping Name"] = data["Shipping Name"].fillna(data["Billing Name"])
+
+    # Columnas que me interesan
+    new_data = data[
+        [
+            "Name",
+            "Paid at",
+            "Currency",
+            "Shipping",
+            "Taxes",
+            "Total",
+            "Shipping Name",
+            "Billing Country",
+        ]
+    ]
+
+    # Cambio de nombres
+    new_data.rename(
+        columns={
+            "Name": "Factura",
+            "Paid at": "Fecha",
+            "Currency": "Moneda",
+            "Shipping": "Envío",
+            "Taxes": "IVA",
+            "Total": "Total",
+            "Shipping Name": "Nombre Cliente",
+            "Billing Country": "País de venta",
+        },
+        inplace=True,
+    )
+
+    # Envio SIN IVA = Envio / 1.21
+    new_data.loc[:, "Envío sin IVA"] = round(new_data["Envío"] / 1.21, 2)
+
+    # Envio sin IVA = (Total - (Envio SIN IVA * 0.21) - Envio SIN IVA) / (1 + 0.21)
+    new_data.loc[:, "Subtotal sin IVA"] = round(
+        (
+            new_data["Total"]
+            - (new_data["Envío sin IVA"] * 0.21)
+            - new_data["Envío sin IVA"]
+        )
+        / (1 + 0.21),
+        2,
+    )
+
+    # Reordenar columnas
+    new_data = new_data[
+        [
+            "Factura",
+            "Fecha",
+            "Moneda",
+            "Subtotal sin IVA",
+            "Envío",
+            "Envío sin IVA",
+            "IVA",
+            "Total",
+            "Nombre Cliente",
+            "País de venta",
+        ]
+    ]
+
+    return new_data
+
+
+if __name__ == "__main__":
+    data = get_data()
+    new_data = transform_data(data)
+    # Elimino duplicados y me quedo con el último
+
+    print(new_data.head())
+    print(len(new_data.index))
+
+    gt_data = pd.read_excel("./OUTPUT/Listado Facturas Septiembre 2024.xlsx")
+    print(gt_data.head())
+    print(len(gt_data.index))
+
+    compare = new_data[
+        ~(new_data["Nombre Cliente"].isin(gt_data["Nombre Cliente"]))
+    ].reset_index(drop=True)
+
+    print(compare)
+    compare.to_csv("./OUTPUT/not_in_gt.csv", index=False)
+    gt_data.to_csv("./OUTPUT/gt_data.csv", index=False)
+    new_data.to_csv("./OUTPUT/new_data.csv", index=False)
